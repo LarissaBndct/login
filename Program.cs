017ff7fa6de185b00bfba8462db154e946409eb8
Larissa Benedicto <125518570+LarissaBndct@users.noreply.github.com>
2023-04-11 17:35:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Digite o seu nome de usuário: ");
+        string usuario = Console.ReadLine();
+        Console.WriteLine("Digite sua senha: ");
+        string senha ="";
+        while(true)
+        {
+            ConsoleKeyInfo tecla = Console.ReadKey();
+            if (tecla.Key == ConsoleKey.Enter)
+            break;
+            else
+            {
+                senha += tecla.KeyChar;
+            }      
+        }
+        if (usuario == "Larissa" && senha == "1234")
+        {
+            Console.WriteLine("Usuário logado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Usuário ou senha incorreto");
+        }
+        Console.WriteLine("Pressione qualquer tecla para sair");
+        Console.ReadKey();
+    }
+}
